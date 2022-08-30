21cba690b15ef2bfcba273f01b3fa925ab6e6e20
Roberto amato neto <66858040+robertoamato@users.noreply.github.com>
2022-08-30 18:22:30 -0300

    curso-angularudemy2022

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'meu-primeiroprojeto';
+  public title: string = "bem vindo";
+
 }
