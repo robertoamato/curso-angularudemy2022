785cdf175e25f2cb0a50a30172f7ef3069e2044b
Roberto amato neto <66858040+robertoamato@users.noreply.github.com>
2022-08-31 18:35:20 -0300

    curso-angularudemy2022

--- src/app/AppComponent.ts
+++ src/app/AppComponent.ts
@@ -1,42 +1,35 @@
-import { Component, OnInit, DoCheck, AfterContentInit, AfterContentChecked, AfterViewChecked } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 
 @Component({
   selector: 'app-root',
   template: `
-  {{valor}}
-  <button (click)="adicionar()">Adicionar</button>
+  <app-title *ngIf="destruir"></app-title>
+  <br>
+  <button (click)="DestruirComponent()">Destruir Componente</button>
   <router-outlet></router-outlet> 
     
   `
 })
-export class AppComponent implements OnInit, DoCheck, AfterContentInit, AfterContentChecked, AfterViewChecked {
-  public valor: number = 1;
- 
+export class AppComponent implements OnInit {
+  
  
   constructor() { }
 
-  public adicionar(): number{
-    return this.valor += 1;
-
-
-  }
+  
  
 
-  ngAfterViewChecked(): void {
-    console.log ("ngAfterViewChecked");
-  }
-  ngAfterContentChecked(): void {
-    console.log("gAfterContentChecked");
-  }
-  ngAfterContentInit(): void {
-    console.log("ngAfterContentInit");
-  }
-  ngDoCheck(): void {
-    console.log("ngDoCheck");
-  }
-  ngOnInit(): void {
-    console.log("ngOnInit");
+  
+  ngOnInit(): void { }
+
+
+  public destruir: boolean  = true;
+
+  public DestruirComponent(){
+    this.destruir = false;
+
 
   }
+
+
 }
