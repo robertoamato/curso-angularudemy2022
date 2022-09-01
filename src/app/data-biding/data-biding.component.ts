c3ab879f9b9f2cc9962eb83e4df8e4cf5f0f181f
Roberto amato neto <66858040+robertoamato@users.noreply.github.com>
2022-09-01 18:13:07 -0300

    curso-angularudemy2022

--- src/app/data-biding/data-biding.component.ts
+++ src/app/data-biding/data-biding.component.ts
@@ -8,15 +8,30 @@ import { Component, OnInit } from '@angular/core';
 export class DataBidingComponent implements OnInit {
  public nome:string = "dener";
  public idade:number = 39;
- public maisUm:number = 39;
+ public maisUm:number = 1;
 
  public negar : boolean = false;
  public srcImage : string = "https://ciclovivo.com.br/wp-content/uploads/2018/10/iStock-536613027.jpg";
  public imagem : string = "proprety blinding"
  public teste : string = "titulo"
+
+ public position : {x: number, y: number } = { x: 0, y: 0 };
+
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(): void { }
+
+public alertaInfo(valor : any){
+  console.log(valor);
+
+}
+  public mouseMoveTeste(valor:any){
+    
+    this.position.x = valor.offsetX;
+    this.position.y = valor.offsetY;
+
+
   }
+
 
 }
