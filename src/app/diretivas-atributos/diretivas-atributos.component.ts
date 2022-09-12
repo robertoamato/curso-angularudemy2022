3959f6189ab171dcc10dcfe9de58bf07f99f5e71
Roberto amato neto <66858040+robertoamato@users.noreply.github.com>
2022-09-12 19:11:29 -0300

    curso-angularudemy2022

--- src/app/diretivas-atributos/diretivas-atributos.component.ts
+++ src/app/diretivas-atributos/diretivas-atributos.component.ts
@@ -8,6 +8,11 @@ import { Component, OnInit } from '@angular/core';
 export class DiretivasAtributosComponent implements OnInit {
   public valor : boolean = true;
   public ngClass : string ="ngClass";
+  public ngStyle : string ="ngStyle";
+  public heightPx: string ="heighPx";
+  public backgroundColor = "backgroundColor"
+  public list: Array <{nome: string }> = [];
+  public nome: string ="";
   constructor() { }
 
   ngOnInit(): void {
@@ -19,10 +24,22 @@ export class DiretivasAtributosComponent implements OnInit {
         this.valor=true;
 
       }
+      if(this.heightPx == "20px"){
+        this.heightPx= "50px";
+        this.backgroundColor="blue";
+      }else {
+        this.heightPx ="20px";
+        this.backgroundColor="red";
+      }
 
     },2000);
-
-
+    
+    
+  }
+  public salvar() {
+    this.list.push({ nome: this.nome });
+    this.nome ="";
+    
   }
 
 }
